8746c62312bbb8d0c0e880a3fd3326e0c362baa3
Fernando_Vazquez <61173776+Chilangdon20@users.noreply.github.com>
2021-11-08 21:20:37 -0600

    actualizacion barra de graficas

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ global datos
 if archivo_cargado is not None:
     print(archivo_cargado)
     try:
-        datos = pd.read_csv(archivo_cargado)
+        datos = pd.read_csv(archivo_cargado).dropna()
+
     except Exception as e:
         print(e)
-        datos = pd.read_excel(archivo_cargado)
+        datos = pd.read_excel(archivo_cargado,keep_default_na=False).dropna()
+
 
 global cols_num
 try:
@@ -36,4 +38,30 @@ except Exception as e:
     st.write("Porfavor suba su archivo a la aplicaión")
 
 
+# seleccion de grafico
 
+sel_graf = st.sidebar.selectbox(label="Selecciona el tipo de gráfico",
+                                options=["barras","histograma"])
+
+try:
+    if sel_graf == 'barras':
+        st.sidebar.subheader("Ajustes")
+        x_val = st.sidebar.selectbox(label="X",options=cols_num)
+        x_val = st.sidebar.selectbox(label="X", options=cols_num)
+
+        especificar_subdato = st.sidebar.checkbox(
+            label="Especificar tipo de dato")
+
+        if especificar_subdato:
+            color_v = st.sidebar.selectbox(label="Color",
+                                               options=cols_numNoNumericas)
+            plot = px.bar(data_frame=datos, x=x_val,
+                              y=y_val, color=color_v)
+
+        else:
+            plot = px.bar(data_frame=datos, x=x_val,
+                              y=y_val)
+
+
+except Exception as e:
+    print(e)
